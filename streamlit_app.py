fc5d0221d8054b80872cc5ff3f71b5acd0c10ea1
Snowflake-joe <119219976+Snowflake-joe@users.noreply.github.com>
2022-11-27 12:05:23 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,5 @@ streamlit.text('\N{bowl with spoon}	Omega 3 & Blueberry Oatmeal')
 streamlit.text('\N{leafy green} Kale, Spinage & Rocket Smoothie')
 streamlit.text('\N{chicken} Hard-boiled Free-Range egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
 streamlit.Header('🍌🥭Build Your Own Fruit Smoothie🥝🍇')
