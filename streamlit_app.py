de0100fdbb523cb6ffb1057d51bee32ccf31f0f3
Snowflake-joe <119219976+Snowflake-joe@users.noreply.github.com>
2022-11-27 12:11:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title("My Mom's New Healthy Diner")
 streamlit.header('Breakfast Favoriates')
 streamlit.text('\N{bowl with spoon}	Omega 3 & Blueberry Oatmeal')
 streamlit.text('\N{leafy green} Kale, Spinage & Rocket Smoothie')
@@ -12,4 +12,9 @@ streamlit.header('🍌🥭Build Your Own Fruit Smoothie🥝🍇')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+#Let's Put a pick list here so they can pick the fruit they want to include
+streamlit.mutliselect("Pick some fruits:" list(my_fruit_list.index))
+
+#display the table on the page
 streamlit.dataframe(my_fruit_list)
